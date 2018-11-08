2d7cc3af102fad9bfde2e137bee5b2d9cfb8f2b9
Max Dauber <max@dauberfamily.com>
2018-11-07 22:27:41 -0600

    changed main location, started main game output
    need to write main game loop and check for winner

--- go_fish.cpp
+++ go_fish.cpp
@@ -1,4 +1,46 @@
 //
 // Created by Max Dauber on 11/7/18.
 //
+#include <iostream>    // Provides cout and cin
+#include <fstream>
+#include <cstdlib>     // Provides EXIT_SUCCESS
+#include "card.h"
+#include "player.h"
+#include "deck.h"
 
+using namespace std;
+
+
+void dealHand(Deck &d, Player &p, int numCards)
+{
+    for (int i=0; i < numCards; i++)
+        p.addCard(d.dealCard());
+}
+
+int main( )
+{
+    int numCards = 5;
+    ofstream outFile;
+    outFile.open("gofish_results.txt");
+
+    Player p1("Joe");
+    Player p2("Jane");
+
+    Deck d;  //create a deck of cards
+    d.shuffle();
+
+    dealHand(d, p1, numCards);
+    dealHand(d, p2, numCards);
+
+    cout << p1.getName() <<" has : " << p1.showHand() << endl;
+    cout << p2.getName() <<" has : " << p2.showHand() << endl;
+
+    outFile.close();
+
+    return EXIT_SUCCESS;
+}
+/*
+bool isWinner(Player &p1, Player &p2){
+
+}
+*/
